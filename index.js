ed1e5404990d7eb29641307642e68ed5b36e82c6
Matheus Carneiro <carneiromatheu@gmail.com>
2026-06-29 00:38:23 +0000

    Commit 6 - Fatura em HTML

--- index.js
+++ index.js
@@ -72,4 +72,20 @@ function gerarFaturaStr (fatura, pecas) {
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
 const faturaStr = gerarFaturaStr(faturas, pecas);
-console.log(faturaStr);
+
+function gerarFaturaHTML(fatura, pecas) {
+  let html = "<html>\n";
+  html += `<p> Fatura ${fatura.cliente} </p>\n`;
+  html += "<ul>\n";
+  for (let apre of fatura.apresentacoes) {
+    html += `<li>  ${getPeca(pecas, apre).nome}: ${formatarMoeda(calcularTotalApresentacao(pecas, apre))} (${apre.audiencia} assentos) </li>\n`;
+  }
+  html += "</ul>\n";
+  html += `<p> Valor total: ${formatarMoeda(calcularTotalFatura(pecas, fatura.apresentacoes))} </p>\n`;
+  html += `<p> Créditos acumulados: ${calcularTotalCreditos(pecas, fatura.apresentacoes)} </p>\n`;
+  html += "</html>";
+  return html;
+}
+
+const faturaHTML = gerarFaturaHTML(faturas, pecas);
+console.log(faturaHTML);
